58f674e6733fe1090531b53d5b5d01e8a87eeba3
Thomas Wright <thomaswright1@gmail.com>
2017-06-22 16:43:02 -0500

    add animations

--- src/components/List.js
+++ src/components/List.js
@@ -3,6 +3,8 @@ import R from "ramda";
 import { Row, Block, Flex, InlineBlock } from "jsxstyle";
 import Gravatar from "react-gravatar";
 import { colorTheme } from "./styleConstants";
+import { Collapse } from "react-collapse";
+import { Motion, spring } from "react-motion";
 
 const Avatar = ({ email }) => (
   <Gravatar email={email} size={40} style={{ borderRadius: 8 }} />
@@ -17,38 +19,58 @@ const Email = ({ email }) => (
 );
 
 const NameAndEmail = ({ name, email }) => (
-  <Block paddingLeft={16}>
+  <Block paddingLeft={16} flex={1}>
     <Name name={name} />
     <Email email={email} />
   </Block>
 );
-const Status = () => <Block />;
 
-const TopRow = ({ name, email }) => (
-  <Row height={82} alignItems={"center"} paddingLeft={20}>
-    <Avatar email={email} />
-    <NameAndEmail {...{ name, email }} />
-    <Status />
+const ChervonIcon = props => (
+  <Block component={"i"} className="fa fa-chevron-right" {...props} />
+);
+
+const Status = ({ expanded }) => (
+  <Row fontSize={18} color={colorTheme.grey4} alignItems={"center"}>
+    Member
+    <Motion style={{ x: spring(expanded ? 90 : 0) }}>
+      {({ x }) => (
+        <ChervonIcon
+          fontSize={14}
+          color={colorTheme.green}
+          margin={8}
+          transform={`rotate(${x}deg)`}
+        />
+      )}
+    </Motion>
   </Row>
 );
 
-const GoogleMap = ({
-  lat,
-  lng,
-}) => (
+const TopRow = ({ name, email, expanded }) => (
+  <Row height={82} alignItems={"center"} paddingLeft={20} paddingRight={36}>
+    <Avatar email={email} />
+    <NameAndEmail {...{ name, email }} />
+    <Status expanded={expanded} />
+  </Row>
+);
+
+const GoogleMap = ({ lat, lng }) => (
   <iframe
     src={`https://www.google.com/maps/embed/v1/view?key=AIzaSyB3y14YDxyXlthqFGH-fxdBda-BfHrRs9c&center=${lat},${lng}&zoom=4`}
-    style={{border: 0, paddingTop: 10, paddingBottom: 10}}
+    style={{ border: 0, paddingTop: 10, paddingBottom: 10 }}
     width={300}
     height={200}
     frameBorder={"0"}
     allowFullScreen
-    >
-  </iframe>
-)
+  />
+);
 
 const Address = ({ street, suite, city, zipcode }) => (
-  <Block color={colorTheme.grey3} fontSize={14} whiteSpace={"pre"} paddingTop={10}>
+  <Block
+    color={colorTheme.grey3}
+    fontSize={14}
+    whiteSpace={"pre"}
+    paddingTop={10}
+  >
     <InlineBlock fontWeight={"bold"}>
       {"Address: "}
     </InlineBlock>
@@ -68,8 +90,9 @@ const BottomRow = ({ address }) => (
       suite={address.suite}
       city={address.city}
       zipcode={address.zipcode}
-      />
-    <GoogleMap lat={address.geo.lat} lng={address.geo.lng}/>
+    />
+    <GoogleMap lat={address.geo.lat} lng={address.geo.lng} />
+
   </Flex>
 );
 
@@ -78,8 +101,10 @@ const Item = ({ name, address, email, expand, expanded }) => (
     props={{ onClick: expand }}
     borderBottom={`solid 1px ${colorTheme.grey2}`}
   >
-    <TopRow {...{ name, email }} />
-    {expanded && <BottomRow address={address} />}
+    <TopRow {...{ name, email, expanded }} />
+    <Collapse isOpened={expanded}>
+      <BottomRow address={address} />
+    </Collapse>
   </Block>
 );
 
